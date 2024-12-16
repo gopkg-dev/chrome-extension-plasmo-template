bdcd3da30ff067f360a5ef88596298a93a5dfc87
Karen <asin1@aliyun.com>
2024-12-16 10:06:50 +0800

    fix

--- src/contents/plasmo-inline.tsx
+++ src/contents/plasmo-inline.tsx
@@ -1,5 +1,6 @@
 import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo";
 import cssText from "data-text:~main.css";
+import sonnerCssText from "data-text:~sonner.css"
 import { Button } from "~components/ui/button";
 import { Toaster } from "~components/ui/sonner";
 import { toast } from "sonner";
@@ -17,15 +18,68 @@ export const getShadowHostId = () => "plasmo-inline-example-unique-id"
 export const getStyle = () => {
     const style = document.createElement("style")
     style.textContent = cssText
-    // style.textContent = cssText.replaceAll(':root', ':host(plasmo-csui)'); 
+        .replaceAll(":root", ":root,:host(plasmo-csui)")
+        .concat(sonnerCssText)
     return style
 }
 
 const PlasmoInline = () => {
     return (
         <>
-            <Button onClick={() => toast("Hello World")} variant="destructive">Click me</Button>
-            <Toaster />
+            <Button onClick={() => toast("Hello World")}>Click me</Button>
+            <Button onClick={() => {
+                toast('Dynamic Position', {
+                    position: 'top-center',
+                });
+                toast.success('My success toast');
+                toast.error('My error toast');
+                toast('My action toast', {
+                    action: {
+                        label: 'Action',
+                        onClick: () => console.log('Action!'),
+                    },
+                });
+                toast('My cancel toast', {
+                    cancel: {
+                        label: 'Cancel',
+                        onClick: () => console.log('Cancel!'),
+                    },
+                });
+                toast.loading('Loading data', {
+                    duration: 5000,
+                });
+                toast.promise(new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve({ name: 'Success' });
+                    }, 1000);
+                }), {
+                    loading: 'Loading...',
+                    success: (data) => {
+                        return `${(data as { name: string }).name} toast has been added`;
+                    },
+                    error: 'Error',
+                });
+            }} className="ml-2">Click me</Button>
+            <Toaster
+                closeButton
+                richColors
+                toastOptions={{
+                    // classNames: {
+                    //     toast: "items-start leading-[20px]",
+                    //     icon: "mt-[3px]",
+                    //     title: "leading-[22px] text-[16px] break-all",
+                    //     description:
+                    //         "leading-[20px] max-h-[150px] overflow-y-auto break-all"
+                    // }
+                }}
+                visibleToasts={Infinity}
+                position="top-right"
+                duration={Infinity}
+                offset={20}
+                gap={14}
+                pauseWhenPageIsHidden={true}
+                expand
+            />
         </>
     )
 }
